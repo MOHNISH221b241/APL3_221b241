2f9ced90d8d135692406a7f3f788e12b51470c91
MOHNISH221b241 <221b241@juetguna.in>
2025-01-31 14:37:32 +0530

    Delete LE1 directory

--- LE1/Problem 2/Child (1).java
+++
@@ -1,5 +0,0 @@
-class Child extends Mother{
-    void show(){
-        System.out.println("this is child class");
-    }
-}
--- LE1/Problem 3/Child (2).java
+++
@@ -1,5 +0,0 @@
-class Child extends Mother{
-    static void show(){
-        System.out.println("this is child class");
-    }
-}
